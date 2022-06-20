f642e435d516a54323aa8b3535911eee9076a289
jegini <jeginilokesh012022@gmail.com>
2022-06-20 23:56:29 +0530

    I HAVE SOME DOUBT ON NULL DATA TYPE

---
+++ day2.js
@@ -0,0 +1,33 @@
+//Note:js data type are dynamic data typeswe did not assigin data type to variable it will automatically decide data type based on value assigin to variable
+//In Js we have 5 types of data types are present
+//1)number (in js we dont have int and float concept)
+//2)srting
+//3) boolean type
+//4)undefine
+//5)null
+var a = 20; //this also number
+var b = 30.5; //this also number
+console.log(typeof a);
+console.log(typeof b)
+    //2)string data type//note we can represent string in single or double quotes
+var c = "loki"
+var d = "murali"
+console.log(typeof c)
+var e = c + d; //concardination
+console.log(e)
+console.log(typeof e)
+var f = 16 + "loki" //even one string present also it act as concardination
+console.log(typeof e)
+    //boolean type(note here small letters only)
+var g = 30;
+var h = 30;
+var i = g == h;
+console.log(typeof i)
+console.log(i)
+    //4)undefined
+var j;
+console.log(j)
+console.log(typeof j)
+    //5)null this is not under stand to me when i use typeof k it shows undefine data type with out showing null
+var k =
+    console.log(typeof k)
